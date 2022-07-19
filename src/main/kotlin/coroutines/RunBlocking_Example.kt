c12eec163170a213500c19a6d24f22109cb99a2e
Raaj Kancan <iamraajkanchan@gmail.com>
2022-07-19 20:28:32 +0530

    ThirdRunBlocking class is added in RunBlocking_Example - Added

--- src/main/kotlin/coroutines/RunBlocking_Example.kt
+++ src/main/kotlin/coroutines/RunBlocking_Example.kt
@@ -34,3 +34,19 @@ class SecondRunBlocking {
         }
     }
 }
+
+class ThirdRunBlocking {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = runBlocking {
+            /* As doWork is defined in the same thread i.e. main so the code inside doWork method is completed first and then println method is executed */
+            doWork()
+            println("World!!!")
+        }
+
+        private suspend fun doWork() {
+            delay(1000L)
+            println("Hello")
+        }
+    }
+}
