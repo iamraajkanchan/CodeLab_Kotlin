e7704d181b47be066a8c7c1312f19924178b4aa1
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-07-24 18:07:50 +0530

    StreamWithSuspendingFunctions is added in JetBrainsOrg_Examples

--- src/main/kotlin/kotlin_flow/JetBrainsOrg_Examples.kt
+++ src/main/kotlin/kotlin_flow/JetBrainsOrg_Examples.kt
@@ -2,6 +2,8 @@ package kotlin_flow
 
 import coroutines.log
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 
 /**
  * Asynchronous Flow : Representing multiple values - List - You can use List if there is no delay
@@ -46,3 +48,23 @@ class StreamWithSequence {
     * */
 }
 
+/**
+ * Asynchronous Flow : Representing Multiple Values - Suspending Functions
+ * */
+class StreamWithSuspendingFunctions {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = runBlocking {
+            simple().forEach { value -> print(value) } // Prints the value of list after 1000 ms
+        }
+
+        private suspend fun simple(): List<Int> {
+            delay(1000L) // halting simple() to return the list
+            return listOf(1, 2, 3, 4, 5)
+        }
+    }
+    /*
+    * Output
+    * 12345
+    * */
+}
