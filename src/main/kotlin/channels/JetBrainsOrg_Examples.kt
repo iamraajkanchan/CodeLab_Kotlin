308f81827edbbeb239845b153d363f16b4d379a4
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-07-28 11:49:57 +0530

    ChannelProducersIntroduction is added in JetBrainsOrg_Examples

--- src/main/kotlin/channels/JetBrainsOrg_Examples.kt
+++ src/main/kotlin/channels/JetBrainsOrg_Examples.kt
@@ -1,6 +1,11 @@
 package channels
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -49,6 +54,34 @@ class ClosingAndIterationOverChannels {
         }
     }
     /*
+    * 1
+    * 4
+    * 9
+    * 16
+    * 25
+    * Done
+    * */
+}
+
+/**
+ * Channel - Build Channel Producers
+ * */
+class ChannelProducersIntroduction {
+    companion object {
+        @JvmStatic
+        fun main(array: Array<String>) = runBlocking {
+            val squares = produceSquares()
+            squares.consumeEach { println(it) }
+            println("Done")
+        }
+
+        @OptIn(ExperimentalCoroutinesApi::class)
+        private fun CoroutineScope.produceSquares(): ReceiveChannel<Int> = produce {
+            for (i in 1..5) send(i * i)
+        }
+    }
+    /*
+    * Output
     * 1
     * 4
     * 9
