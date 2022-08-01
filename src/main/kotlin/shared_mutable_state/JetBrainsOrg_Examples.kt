ad5514823f2c8693faf1a15f7f38fdeb330ac6aa
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-08-01 15:25:25 +0530

    UseVolatileInsteadOfSharedMutableState is added in JetBrainsOrg_Examples

--- src/main/kotlin/shared_mutable_state/JetBrainsOrg_Examples.kt
+++ src/main/kotlin/shared_mutable_state/JetBrainsOrg_Examples.kt
@@ -45,3 +45,49 @@ class ProblemWithoutSharedMutableState {
     * Counter = 95108
     * */
 }
+
+/**
+ * Shared mutable state and concurrency : Volatiles are of no help - This code works slower, but we
+ * still don't get "Counter = 100000" at the end, because volatile variables guarantee linearizable
+ * (this is a technical term for "atomic") reads and writes to the corresponding variable, but do
+ * not provide atomicity of larger actions (increment in our case).
+ * */
+class UseVolatileInsteadOfSharedMutableState {
+    companion object {
+
+        @Volatile
+        private var counter = 0 // marked the counter as volatile
+
+        @JvmStatic
+        fun main(args: Array<String>) = runBlocking {
+            withContext(Dispatchers.Default) {
+                massiveRun {
+                    counter++
+                }
+            }
+            println("Counter $counter")
+        }
+
+        private suspend fun massiveRun(action: suspend () -> Unit) {
+            val n = 100
+            val k = 1000
+            val time = measureTimeMillis {
+                coroutineScope {
+                    repeat(n) {
+                        launch {
+                            repeat(k) {
+                                action()
+                            }
+                        }
+                    }
+                }
+            }
+            println("Completed ${n * k} actions in $time ms")
+        }
+    }
+    /*
+    * Output
+    * Completed 100000 actions in 50 ms
+    * Counter 95448
+    * */
+}
