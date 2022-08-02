2d07d2e7e3a24470d3e659e75a582ffea5d46ae6
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-08-02 10:57:30 +0530

    ImmutableCollectionExample is added in JetBrainsOrg_Examples (collections)

--- src/main/kotlin/collections/JetBrainsOrg_Examples.kt
+++ src/main/kotlin/collections/JetBrainsOrg_Examples.kt
@@ -23,3 +23,37 @@ class CollectionTypes {
     * one two three four five
     * */
 }
+
+/**
+ * Collection Overview - Collection Types - Collection -
+ * List and Set interfaces implements Collection interface
+ * */
+class ImmutableCollectionExample {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val numberList = listOf<String>("one", "two", "four", "five", "one", "three")
+            println("Printing List")
+            printAll(numberList) // Printing a List of String
+            val numberSet = setOf<String>("one", "two", "four", "five", "one", "three")
+            println("Printing Set")
+            printAll(numberSet) // Printing a Set of String
+        }
+
+        /**
+         * printAll method is used to print elements of a List of Set.
+         * @param strings can take List and Set type of variables.
+         * */
+        private fun printAll(strings: Collection<String>) {
+            strings.forEach { print("$it ") }
+            println()
+        }
+    }
+    /*
+    * Output
+    * Printing List
+    * one two four five one three
+    * Printing Set
+    * one two four five three
+    * */
+}
