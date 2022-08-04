a6a55a86155e486377957158789e80390b62cb41
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-08-04 12:58:53 +0530

    InitBlockExample is updated in JetBrainsOrg_Examples (classes)

--- src/main/kotlin/classes/JetBrainsOrg_Examples.kt
+++ src/main/kotlin/classes/JetBrainsOrg_Examples.kt
@@ -1,29 +1,59 @@
 package classes
 
-data class Student(val name: String? = null, val rollNo: Int? = null, val className: String? = null)
+data class Student(val name: String? = null, var rollNo: Int? = null, var className: String? = null)
 
 class Person(private val student: Student? = null) {
     init {
-        displayDetails()
+        displayStudentDetails()
     }
-    private fun displayDetails() {
+
+    private fun displayStudentDetails() {
         student?.also {
             println("Name: ${it.name}")
             println("Roll No: ${it.rollNo}")
             println("Class Name: ${it.className}")
         }
     }
+
+    fun updateClassName(newClassName: String) {
+        student?.className = newClassName
+        displayStudentDetails()
+    }
+
+    fun updateRollNumber(newRollNo: Int) {
+        student?.rollNo = newRollNo
+        displayStudentDetails()
+    }
 }
 
 /**
  * Classes - Constructors
  * */
-class InitBlockExample {
+class InitBlockExample(name: String) {
+
+    val firstProperty = "First property: $name".also(::println)
+
+    init {
+        println("First initializer block that prints $name")
+    }
+
+    val secondProperty = "Second property: ${name.length}".also(::println)
+
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val john = Student("John Doe", 22, "XII")
-            val person = Person(john)
+            InitBlockExample("Raj Kanchan")
         }
     }
+    /*
+    * Output
+    * First property: Raj Kanchan
+    * First initializer block that prints Raj Kanchan
+    * Second property: 11
+    * Second initializer block that prints 11
+    * */
 }
