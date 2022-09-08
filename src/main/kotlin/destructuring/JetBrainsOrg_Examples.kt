1b50998c346a47fd11d43f4aa34e6fac33399e22
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-09-08 23:47:29 +0530

    AwaitingMultipleCoroutines is updated in Medium_Examples.kt (coroutines)

--- src/main/kotlin/destructuring/JetBrainsOrg_Examples.kt
+++ src/main/kotlin/destructuring/JetBrainsOrg_Examples.kt
@@ -2,20 +2,26 @@ package destructuring
 
 /**
  * Destructuring Declarations - Basic Destructuring
+ * Using Object of a Class and componentN method of Object.
  * */
 class BasicDestructuring {
-    data class Person(val name: String, val age: Int)
+    data class Teacher(val name: String, val age: Int)
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val teacher = Person("Claire Mathias", 24)
+            val historyTeacher = Teacher("Claire Mathias", 24)
             /* To de-structure the properties of a class ma*/
-            val (name, age) = teacher
-            println("Name of the teacher is $name, who is just $age year old.")
+            val (historyTeacherName, historyTeacherAge) = historyTeacher
+            println("Name of the History teacher is $historyTeacherName, who is just $historyTeacherAge year old.")
+            val civicsTeacher = Teacher("James Chad", 28)
+            val civicsTeacherName = civicsTeacher.component1()
+            val civicsTeacherAge = civicsTeacher.component2()
+            println("Name of the Civics teacher is $civicsTeacherName, who is just $civicsTeacherAge year old.")
         }
     }
     /*
     * Output
-    * Name of the teacher is Claire Mathias, who is just 24 year old.
+    * Name of the History teacher is Claire Mathias, who is just 24 year old.
+    * Name of the Civics teacher is James Chad, who is just 28 year old.
     * */
 }
