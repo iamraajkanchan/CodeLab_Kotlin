bef939821ede34332142e73dde30dd865a265cb2
iamraajkanchan <iamraajkanchan@gmail.com>
2022-11-19 20:40:12 +0530

    WithoutEncapsulationExamplePersonInOffice.kt and WithoutEncapsulationExamplePersonInOffice.kt is updated.

--- src/main/kotlin/classes/encapsulation/without_enapsulation/WithoutEncapsulationExamplePersonRecord.kt
+++ src/main/kotlin/classes/encapsulation/without_enapsulation/WithoutEncapsulationExamplePersonRecord.kt
@@ -6,6 +6,7 @@ class WithoutEncapsulationExamplePersonRecord {
         fun main(args: Array<String>) {
             // val office =  WithoutEncapsulationExamplePersonInOffice.getInstance()
             // It is not feasible to get the instance of office which is defined in WithoutEncapsulationExamplePersonInOffice
+            // UserName of the Git is changed.
         }
     }
 }
